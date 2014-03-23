037efc2a0e099d6d2b378b03a81f60076449a532
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 18:05:57 -0700

    Get ball displaying on canvas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 var states = { preload, create, update }
 var game = new Phaser.Game(400, 300, Phaser.AUTO, 'phaser-game', states)
 
-var paddle
+var ball, paddle
 
 function preload() {
+  game.load.image('ball', 'ball.png')
   game.load.image('paddle', 'paddle.png')
 }
 
 function create() {
+  ball = game.add.sprite(game.world.centerX, 0, 'ball')
+
   paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
   paddle.anchor.setTo(0.5, 0.5)
 }
