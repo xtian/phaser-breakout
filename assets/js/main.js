1ebae71b66800e39f66b60bedd34a602c589e5c8
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 17:50:45 -0700

    Get paddle displaying on canvas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 var states = { preload, create, update }
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', states)
+var game = new Phaser.Game(400, 300, Phaser.AUTO, 'phaser-game', states)
 
 function preload() {
-
+  game.load.image('paddle', 'paddle.png')
 }
 
 function create() {
-
+  var paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
+  paddle.anchor.setTo(0.5, 0.5)
 }
 
 function update() {
