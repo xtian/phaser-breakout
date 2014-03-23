a1a4164f82fc433b89de92bbe54ca574f2ab82d9
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 17:57:22 -0700

    Paddle moves with mouse.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 var states = { preload, create, update }
 var game = new Phaser.Game(400, 300, Phaser.AUTO, 'phaser-game', states)
 
+var paddle
+
 function preload() {
   game.load.image('paddle', 'paddle.png')
 }
 
 function create() {
-  var paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
+  paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
   paddle.anchor.setTo(0.5, 0.5)
 }
 
 function update() {
-
+  paddle.x = game.input.x
 }
