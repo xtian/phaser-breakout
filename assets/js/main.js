d80d6e1a930aab26d694ceb749522f3f465f7706
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 18:39:08 -0700

    Get ball colliding with paddle, but not well.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,15 +12,34 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
   ball = game.add.sprite(game.world.centerX, 0, 'ball')
-  game.physics.enable(ball, Phaser.Physics.ARCADE)
+  paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
+
+  game.physics.enable([ball, paddle], Phaser.Physics.ARCADE)
+
   ball.body.velocity.setTo(200, 200)
   ball.body.collideWorldBounds = true
   ball.body.bounce.set(1)
 
-  paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
   paddle.anchor.setTo(0.5, 0.5)
+  paddle.body.immovable = true
 }
 
 function update() {
-  paddle.x = game.input.x
+  paddle.body.x = game.input.x
+
+  game.physics.arcade.overlap(ball, paddle, collisionHandler, null, this)
+}
+
+function collisionHandler(ball, paddle) {
+  var diff = 0
+
+  if (ball.x < paddle.x) {
+    diff = paddle.x - ball.x
+    ball.body.velocity.x = (-10 * diff)
+  } else if (ball.x > paddle.x) {
+    diff = ball.x - paddle.x
+    ball.body.velocity.x = (10 * diff)
+  } else {
+    ball.body.velocity.x = 2 + Math.random() * 8
+  }
 }
