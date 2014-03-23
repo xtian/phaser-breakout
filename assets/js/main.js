389934dae32b1d09a626305ff4e1458a5b26e838
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 19:22:55 -0700

    Get brick displaying on canvas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,30 @@
 var states = { preload, create, update }
 var game = new Phaser.Game(400, 300, Phaser.AUTO, 'phaser-game', states)
 
-var ball, paddle
+var ball, brick, paddle
 var leftKey, rightKey
 
 function preload() {
   game.load.image('ball', 'ball.png')
+  game.load.image('brick', 'brick.png')
   game.load.image('paddle', 'paddle.png')
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
-  ball = game.add.sprite(game.world.centerX, 0, 'ball')
-  paddle = game.add.sprite(game.world.centerX, 280, 'paddle')
+  ball = game.add.sprite(game.world.centerX, 50, 'ball')
+  brick = game.add.sprite(game.world.centerX - 25, 20, 'brick')
+  paddle = game.add.sprite(game.world.centerX - 25, 280, 'paddle')
 
-  game.physics.enable([ball, paddle], Phaser.Physics.ARCADE)
+  game.physics.enable([ball, brick, paddle], Phaser.Physics.ARCADE)
 
   ball.body.velocity.setTo(200, 200)
   ball.body.collideWorldBounds = true
   ball.body.bounce.set(1)
+
+  brick.anchor.setTo(0, 0.5)
+  brick.body.immovable = true
 
   paddle.anchor.setTo(0, 0.5)
   paddle.body.collideWorldBounds = true
@@ -37,9 +42,14 @@ function update() {
     paddle.body.x += 10
   }
 
-  game.physics.arcade.overlap(ball, paddle, collisionHandler, null, this)
+  game.physics.arcade.overlap(ball, brick, brickHandler, null, this)
+  game.physics.arcade.overlap(ball, paddle, paddleHandler, null, this)
 }
 
-function collisionHandler(ball, paddle) {
+function brickHandler(ball, brick) {
+  brick.kill()
+}
+
+function paddleHandler(ball, paddle) {
   ball.body.velocity.y *= -1
 }
