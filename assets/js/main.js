0c4fabc769e3ae70f1e5907787a502736b386f54
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 18:11:46 -0700

    Get ball bouncing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,13 @@ function preload() {
 }
 
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE)
+
   ball = game.add.sprite(game.world.centerX, 0, 'ball')
+  game.physics.enable(ball, Phaser.Physics.ARCADE)
+  ball.body.velocity.setTo(200, 200)
+  ball.body.collideWorldBounds = true
+  ball.body.bounce.set(1)
 
   paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'paddle')
   paddle.anchor.setTo(0.5, 0.5)
