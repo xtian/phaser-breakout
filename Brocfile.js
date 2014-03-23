f868736a63b799820f99dea6d2eba39d6b91363e
Christian Wesselhoeft <christian@cwesselhoeft.com>
2014-03-22 17:16:34 -0700

    Get blank game displaying.

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,19 @@
 module.exports = function(broccoli) {
+  var pickFiles = require('broccoli-static-compiler')
+  var traceur = require('broccoli-traceur')
+
+  var jsFiles = broccoli.makeTree('assets/js')
+  jsFiles = traceur(jsFiles, { blockBinding: true })
+  jsFiles = pickFiles(jsFiles, { srcDir: '/', destDir: 'dist' })
+
   var publicFiles = broccoli.makeTree('public')
 
-  return [publicFiles]
+  var vendorFiles = new broccoli.MergedTree(broccoli.bowerTrees())
+  vendorFiles = pickFiles(vendorFiles, {
+    files: ['**/*.js',]
+  , srcDir: '/'
+  , destDir: 'dist'
+  })
+
+  return [jsFiles, publicFiles, vendorFiles]
 }
---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var states = { preload, create, update }
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', states)
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
